7f64984c837a5a123584307f939c5a01db9862d3
Aaroh Bhardwaj <z0diac@Aarohs-MacBook-Pro.local>
2025-11-14 03:32:09 +0530

    first commit

---
+++ identity-service/src/utils/Logger.js
@@ -0,0 +1,20 @@
+const winston = require('winston');
+
+const logger = winston.createLogger({
+    level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+    format : winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.splat(),
+        winston.format.errors({ stack: true }),
+        winston.format.json()
+    ),
+    defaultMeta: { service: 'identity-service' },   
+    transports: [
+        new winston.transports.Console({
+            format: winston.format.combine([
+                winston.format.colorize(),
+                winston.format.simple()
+            ])
+        })
+    ]
+})
